7ce5215c19a4995104bb89ab95a08069b33611b8
malmidevindi26git config --global user.name <malmidevindi26@gmail.com>
2026-06-07 12:30:29 +0530

    user-profile

--- src/Routers/authRouter.ts
+++ src/Routers/authRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { creatUser, getMyDetails, login } from "../controllers/authController";
+import { changePassword, creatUser, deleteAccount, getMyDetails, login, updateProfilePicture } from "../controllers/authController";
 import { authenticate } from "../middleware/auth";
 
 const router = Router()
@@ -7,5 +7,8 @@ const router = Router()
 router.post("/register", creatUser)
 router.post("/login", login)
 router.get("/me", authenticate,getMyDetails)
+router.put("/profile-pic", authenticate, updateProfilePicture)
+router.put("/change-password", authenticate, changePassword)
+router.delete("/delete-account", authenticate, deleteAccount)
 
 export default router
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,6 +3,8 @@ import { Request, Response } from "express"
 import bcrypt from "bcryptjs"
 import { signAccessToken, signRefreshToken } from "../utils/token"
 import { AuthRequest } from "../middleware/auth"
+import mongoose from "mongoose"
+import { FavoriteCityModel } from "../models/favoriteModel"
 
 // user registration
 export const creatUser = async (req:Request, res:Response) => {
@@ -83,4 +85,115 @@ export const getMyDetails = async (req:AuthRequest, res: Response) =>{
     }
 
     res.status(200).json({message: "Ok", data:{ id: user._id, email: user.email, roles: user.roles }})
+}
+
+// update profile pic
+export const updateProfilePicture = async (req: AuthRequest, res:Response) => {
+ 
+ if(!req.user){
+  return res.status(401).json({message:"Unauthorized access"})
+ }
+
+  const {profilePicUrl} = req.body
+
+  const userId = req.user.sub // user id coming from middleware
+
+  try{
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      {profilePicture: profilePicUrl},
+      {new:true}
+    ).select("-password")
+    if(!updatedUser){
+      return res.status(404).json({message: "User not found"})
+    }
+
+    res.status(200).json({message: "Profile picture updated successfully", user:updatedUser})
+  }catch(err){
+    console.error(err)
+    res.status(500).json({message: "Failed to update profile picture"})
+  }
+}
+
+// change password
+export const changePassword = async(req:AuthRequest, res:Response) =>{
+  if(!req.user){
+  return res.status(401).json({message:"Unauthorized access"})
+  }
+
+  const {oldPassword, newPassword} = req.body
+  const userId = req.user.sub
+
+  if(!oldPassword || !newPassword){
+    return res.status(400).json({message: "Both old and new passwords required"})
+  }
+
+  try {
+    const user = await UserModel.findById(userId)
+    if(!user){
+      return res.status(404).json({message:"user not found"})
+    }
+
+    //compare old password
+    const isMatch = await bcrypt.compare(oldPassword, user.password)
+    if(!isMatch){
+      return res.status(400).json({message:"Incorrect old password"})
+    }
+
+    // save new password as a hash
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(newPassword, salt)
+    await user.save();
+
+    res.status(200).json({message: "Password changes successfully"})
+    
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({message: "Failed to change password"})
+  }
+} 
+
+// delete acc
+export const deleteAccount = async (req:AuthRequest, res:Response) => {
+  if(!req.user){
+  return res.status(401).json({message:"Unauthorized access"})
+  }
+
+  const userId = req.user.sub
+
+  // start mongoose session
+  const session = await mongoose.startSession()
+
+  try {
+    // start transaction
+    session.startTransaction()
+
+    // delete fav cities
+    await FavoriteCityModel.deleteMany({user:userId}).session(session)
+
+    // delete user acc
+    const deleteUser = await UserModel.findByIdAndDelete(userId).session(session)
+
+    if(!deleteUser){
+      // Transaction abort, if there is no user
+      await session.abortTransaction()
+      session.endSession()
+      return res.status(404).json({message: "User account not found"})
+    }
+
+    // commit database if all are success
+    await session.commitTransaction()
+    session.endSession()
+    
+    res.status(200).json({
+      message: "Account and all associates favorite weather data deleted successfully !"
+    })
+  } catch (error) {
+    // rollback
+    await session.abortTransaction()
+    session.endSession()
+
+    console.error("Delete account transaction error",error)
+    res.status(500).json({message:"Failed to delete account safely, Chnage rolled back"})
+  }
 }
