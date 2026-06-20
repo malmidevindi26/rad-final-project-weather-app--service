cd20472a61dc9bcd6bee702c2caebceea2c1a742
malmidevindi26git config --global user.name <malmidevindi26@gmail.com>
2026-06-21 01:55:23 +0530

    fix

--- src/models/weatherLogModel.ts
+++ src/models/weatherLogModel.ts
@@ -15,9 +15,9 @@ const WeatherLogSchema = new Schema<IWeatherLog>(
     {
         title: { type: String, required: true },
         description: { type: String, required: true },
-        cityName: { type: String, required: true },  // 👈 එකතු කළා
-        temp: { type: Number, required: true },       // 👈 එකතු කළා
-        humidity: { type: Number, required: true },   // 👈 එකතු කළා
+        cityName: { type: String, required: true },  
+        temp: { type: Number, required: true },      
+        humidity: { type: Number, required: true },  
         cityId: { type: Schema.Types.ObjectId, ref: "FavoriteCity" },
         user: { type: Schema.Types.ObjectId, ref: "user_details" }
     },
